5cacd4079f8e79599a099297185712ce5d140f8b
Eduardo Brito <up201806271@fe.up.pt>
2021-01-22 18:24:07 +0000

    Added more Code & Docs

---
+++ chapter-10/MovingRectangles/RectangleViewer.java
@@ -0,0 +1,13 @@
+package MovingRectangles;
+
+import MovingRectangles.RectangleFrame;
+import javax.swing.JFrame;
+
+public class RectangleViewer {
+    public static void main(String[] args) {
+        JFrame frame = new RectangleFrame();
+        frame.setTitle("An animated rectangle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
