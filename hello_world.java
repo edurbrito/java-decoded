3b0c8be63623c90ddf06ddbac2c1de0b8a60fdde
Eduardo Brito <up201806271@fe.up.pt>
2021-01-22 18:21:29 +0000

    Added Utils & Hello World

---
+++ hello_world.java
@@ -0,0 +1,10 @@
+public class hello_world {
+    static void print(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        print("Hello world");
+    }
+
+}
